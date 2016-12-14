9e6a74806c00256f278e564913dd9346cdb71c9f
Marcel Wysocki <maci.stgn@gmail.com>
2016-12-14 13:01:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -31,18 +31,18 @@ function test() {
 function handler() {
     $request = \Slim\Slim::getInstance()->request();
     $body = $request->getBody();
-    $input = json_decode($body); 
-    echo json_encode($body);
+    //$input = json_decode($body); 
+    //echo json_encode($body);
 
     //$data = getenv('data');
     $payload_data = new stdClass();
     $payload_data->serviceName = $request->post('serviceName');
-    //$payload_data->payload->teamName = $request->post('$teamName');
-    //$payload_data->payload->reindeerName = $request->post('$reindeerName');
-    //$payload_data->payload->nameEmaiMap1 = $request->post('$nameEmaiMap1');
-    //$payload_data->payload->nameEmaiMap2 = $request->post('$nameEmaiMap2');
+    $payload_data->payload->teamName = $request->post('teamName');
+    $payload_data->payload->reindeerName = $request->post('reindeerName');
+    $payload_data->payload->nameEmaiMap1 = $request->post('nameEmaiMap1');
+    $payload_data->payload->nameEmaiMap2 = $request->post('nameEmaiMap2');
 
-    //echo json_encode($payload_data);
+    echo json_encode($payload_data);
 
     $dir = new DirectoryIterator("/etc/santas-config/..data/");
     foreach ($dir as $fileinfo) {
