c3f3334038a4da4aa1707e8660b5acf2b223e571
Marcel Wysocki <maci.stgn@gmail.com>
2016-12-14 16:54:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -63,6 +63,7 @@ function handler() {
     foreach ($reindeers as $key => $reindeer) {
         var_dump ($key);
         var_dump ($reindeer);
+        $payload_data->payload[$key] = new stdClass();
         $payload_data->payload[$key]->teamName = $data->payload[0]->teamName;
         $payload_data->payload[$key]->reindeerName = $reindeer;
         $payload_data->payload[$key]->nameEmaiMap = $data->payload[0]->nameEmaiMap;
